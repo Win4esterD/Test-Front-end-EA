806795911f8405ebc416c11f99f6ee4d950cb50c
Pavel Didenko <win4ester2016@yandex.ru>
2023-02-22 17:22:16 +0300

    fix: improve timer to make it stop when the time runs out

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -4,55 +4,79 @@ class CountDown extends React.Component{
   targetDate = new Date(2023, 4, 31);
 
   state = {
-    seconds: 0
+    seconds: this.secondsNow()
   }
 
 
   componentDidMount(){
     const seconds = document.querySelector('#seconds');
     seconds.innerHTML = `${this.secondsNow()}`;
-    setInterval(() => {
+    const interval = setInterval(() => {
       this.setSeconds();
       seconds.innerHTML = `${this.state.seconds}`;
-    }, 1000)
+    }, 1000);
+
+    if(!this.checkDate()){
+      clearInterval(interval);
+    };
+  }
+
+  checkDate(){
+    return this.targetDate > new Date()
   }
 
 
   countDays(){
-    const daysLeft = Math.floor(Math.abs(this.targetDate - new Date())/(1000*60*60*24));
-    return daysLeft;
+    if(this.checkDate()){
+      const daysLeft = Math.floor(Math.abs(this.targetDate - new Date())/(1000*60*60*24));
+      return daysLeft;
+    }else{
+      return '0';
+    }
   }
 
   countHours(){
-    const currentDate = new Date();
-    let hours
-    if(currentDate.getMinutes() === 0 && currentDate.getSeconds() === 0){
-      hours = 24 - currentDate.getHours();
-    }else{
-      hours =  24 - currentDate.getHours() - 1;
-    }
+      if(this.checkDate()){
+        const currentDate = new Date();
+        let hours
+        if(currentDate.getMinutes() === 0 && currentDate.getSeconds() === 0){
+          hours = 24 - currentDate.getHours();
+        }else{
+          hours =  24 - currentDate.getHours() - 1;
+        }
 
-    if(hours > 9){
-      return String(hours);
-    }else{
-      return `0${hours}`;
+        if(hours > 9){
+          return String(hours);
+        }else{
+          return `0${hours}`;
+        }
+      }else{
+        return '0';
+      }
     }
-  }
 
   getMinutes(){
-    if(59 - new Date().getMinutes() > 9){
-      return 59 - new Date().getMinutes();
+    if(this.checkDate()){
+      if(59 - new Date().getMinutes() > 9){
+        return 59 - new Date().getMinutes();
+      }else{
+        return `0${59 - new Date().getMinutes()}`;
+      }
     }else{
-      return `0${59 - new Date().getMinutes()}`;
+      return '0';
     }
     
   }
 
   secondsNow(){
-    if(59 - new Date().getSeconds() > 9){
-      return 59 - new Date().getSeconds();
+    if(this.checkDate()){
+      if(59 - new Date().getSeconds() > 9){
+        return 59 - new Date().getSeconds();
+      }else{
+        return `0${59 - new Date().getSeconds()}`;
+      }
     }else{
-      return `0${59 - new Date().getSeconds()}`;
+      return '00';
     }
   }
 
